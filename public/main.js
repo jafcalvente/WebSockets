b4c1425dfda2fce1dd6caf2cec5c507ecd17c92c
Jose Fuentes <jafcalvente@outlook.com>
2016-07-19 00:31:54 +0300

    Messages from a client to the server and then from the server to all clients. It's a chat application.

--- public/main.js
+++ public/main.js
@@ -1,17 +1,43 @@
 // Connecting with socket using library socket.io imported on index page
 var socket = io.connect('http://localhost:8080', { 'forceNew': true });
 
-socket.on('messages', function(data) {
+/**
+ * Received message from server.
+ */
+socket.on('server-messages', function(data) {
 	console.log(data);
 	render(data);
 });
 
 var render = function(data) {
 
-	var html = `<div>
+	// Use map function to create a div for each item and then join all of them
+	var html = data.map(function(data, index) {
+		return	`<div>
 					<strong>${data.author}</strong>:
 					<em>${data.text}</em>
 				</div>`;
+	}).join('');
 
 	document.getElementById('messages').innerHTML = html;
+};
+
+/**
+ * Send message to server.
+ */
+var addMessage = function(e) {
+	var infoToSend = {
+		author: document.getElementById('username').value,
+		text: document.getElementById('text').value
+	};
+
+	// Clean textboxes
+	document.getElementById('username').value = '';
+	document.getElementById('text').value = '';
+
+	// Send by socket the objet
+	socket.emit('client-message', infoToSend);
+
+	// Return false to not refresh the page
+	return false
 };
--- server/main.js
+++ server/main.js
@@ -2,6 +2,12 @@
 var express = require('express');
 var app = express();
 
+// This array stores the messages
+var messages = [{
+		author: 'jafcalvente',
+		text: 'This is a message'
+	}];
+
 // Let's use the Server method of http library instead of express as
 // web server because Socket.io works better with this kind of server
 var server = require('http').Server(app);
@@ -13,19 +19,24 @@ app.get('/', function(req, res) {
 	res.status(200).send("Hello world!");
 });
 
-
 // Run socket.io library
 var io = require('socket.io')(server);
 
-// Defines what it's going to do when the application receives the message 'connection'
+// Defines what it's going to happen when the application receives the message of connection
 io.on('connection', function(socket) {
 	console.log('Someone is connected by socket.');
 
-	// Now we can send information using the key messages
-	socket.emit('messages', {
-		id: 1,
-		text: 'This is a message',
-		author: 'jafcalvente'
+	// Now we can send information using the key server-messages. Client should be listening to this key
+	socket.emit('server-messages', messages);
+
+	// Server will be listening to messages that use the key client-message
+	socket.on('client-message', function(data) {
+
+		// Push the new message into the storage array
+		messages.push(data);
+
+		// This method emit the message to all connected clients through the key server-messages
+		io.sockets.emit('server-messages', messages);
 	});
 })
 
